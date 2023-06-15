80e7a81d2e00015735fe442ca9bdb1b634c62907
igorlech <eaaigle@students.eaaa.dk>
2023-06-15 14:39:37 +0200

    restructured styling files

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.scss";
 import BrowserRouter from "./components/BrowserRouter.jsx";
 import ScrollToTop from "./components/ScrollToTop.jsx";
+import "./styles/variables.scss";
+import "./styles/reset.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
