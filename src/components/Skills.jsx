1537a5318240bbf6b237df03b8d24de6db09f7cd
igorlech <eaaigle@students.eaaa.dk>
2023-06-18 12:21:48 +0200

    headings styling, HeroBanner and Skills correction

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,4 +1,5 @@
 import "../styles/skills.scss";
+import codeIcon from "../assets/code-icon.png";
 
 import { Fade } from "react-awesome-reveal";
 
@@ -7,8 +8,11 @@ export default function Skills() {
     <section className="skills-section">
       <Fade triggerOnce="true" direction="up">
         <h2 className="skills-heading heading" id="skills-heading">
-          my main skills
+          my skills
         </h2>
+        <div className="skills-icon-cont">
+          <img className="skills-icon" src={codeIcon}></img>
+        </div>
       </Fade>
     </section>
   );
