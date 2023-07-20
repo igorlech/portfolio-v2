e803808d545a3f96e5175433f2e68045db049248
igorlech <eaaigle@students.eaaa.dk>
2023-07-20 18:35:29 +0200

    skills grid updated

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,15 +1,17 @@
 export default function Technologies() {
   return (
     <div className="skills-technologies">
-      <div className="single-technology">Single Technology</div>
-      <div className="single-technology">Single Technology</div>
-      <div className="single-technology">Single Technology</div>
-      <div className="single-technology">Single Technology</div>
-      <div className="single-technology">Single Technology</div>
-      <div className="single-technology">Single Technology</div>
-      <div className="single-technology">Single Technology</div>
-      <div className="single-technology">Single Technology</div>
-      <div className="single-technology">Single Technology</div>
+      <div className="single-technology">HTML</div>
+      <div className="single-technology">CSS/SCSS</div>
+      <div className="single-technology">JavaScript</div>
+      <div className="single-technology">TypeScript</div>
+      <div className="single-technology">React</div>
+      <div className="single-technology">Webpack</div>
+      <div className="single-technology">Node.js</div>
+      <div className="single-technology">RxJS</div>
+      <div className="single-technology">C# + Razor</div>
+      <div className="single-technology">Umbraco CMS</div>
+      <div className="single-technology">Wordpress CMS</div>
     </div>
   );
 }
